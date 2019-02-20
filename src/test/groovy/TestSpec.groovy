7096c691c2e72a9448dacc7c7435ff07357b8954
Jan Monterrubio <Jan.Monterrubio@Cerner.com>
2019-02-19 22:10:38 -0600

    set unformatted

--- src/test/groovy/TestSpec.groovy
+++ src/test/groovy/TestSpec.groovy
@@ -2,40 +2,33 @@ import spock.lang.*
 
 class TestSpec extends Specification {
 
-  @Unroll
-  def "Test downstream build"() {
+	@Unroll
+	def "Test downstream build"() {
 
-    given:
-    println 'nothing'
+		given:
+		println 'nothing'
 
-    when:
-    println 'something'
+		when:
+		println 'something'
 
-    then:
-    println 'nothing #DOWNSTREAM_STEP'
+		then:
+		println 'nothing #DOWNSTREAM_STEP'
 
-    where:
+		where:
 
-    DOWNSTREAM                                                                      |DOWNSTREAM_STEP
-    [
-      [job: "../some-job/some-branch",parameters: []]]
-    | true
-    [
-      [job: "../some-job/master", parameters: [[stuff: "blah"]], wait: false ]]
-    | true
-    [
-      [job: "../some-job/dev"]]
-    | true
-    [
-      [job: "../some-job/master"],
-      [job: "../another-one/master"]]
-    | true
-    [
-      [job: "     ", parameters: "param", wait: true]]
-    | false
-    [
-      [job: "", wait: true]]
-    | false
-    null                                                                            | false
-  }
+		DOWNSTREAM                                                                      |DOWNSTREAM_STEP
+		[[job: "../some-job/some-branch",parameters: []]] | true
+		[
+			[job: "../some-job/master", parameters: [[stuff: "blah"]], wait: false ]] | true
+		[
+			[job: "../some-job/dev"]] | true
+		[
+			[job: "../some-job/master"],
+			[job: "../another-one/master"]] | true
+		[
+			[job: "     ", parameters: "param", wait: true]] | false
+		[
+			[job: "", wait: true]] | false
+		null                                                                            | false
+	}
 }
